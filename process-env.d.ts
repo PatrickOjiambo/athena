57d6d022db705599218c6addaea198eaba79550e
Roman Njoroge <roman.njoroge@njuguna.com>
2025-05-24 15:33:00 +0300

    build: Describe env variables

---
+++ process-env.d.ts
@@ -0,0 +1,12 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            OKX_API_URL: string;
+            OKX_SECRET_KEY: string;
+            OKX_API_KEY: string;
+            OKX_PASSPHRASE: string;
+        }
+    }
+}
+
+export {}
