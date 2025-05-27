d4a4ce8e123fdc7afce80de276e25629a5c0ae3c
mbeka02 <antonymbeka@gmail.com>
2025-05-27 12:18:43 +0300

    a ton of bug fixes

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,15 @@
 "use client";
-import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 const NotFound = () => {
-  const location = useLocation();
-
+  const pathname = usePathname();
   useEffect(() => {
     console.error(
       "404 Error: User attempted to access non-existent route:",
-      location.pathname
+      pathname,
     );
-  }, [location.pathname]);
+  }, [pathname]);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
